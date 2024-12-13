cbb55b156cee236154a908f621075f321a09d4d7
Maciej Kromka <32202391+kroma2004@users.noreply.github.com>
2024-12-13 22:37:14 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() { }
+
+    public string Info => $"{Name}, Level: {Level}";
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi! I'm {Name}, level {Level}.");
+    }
+}
